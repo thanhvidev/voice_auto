7be036e6458ab1a13986eab5a028a4b527551d01
Nguyễn Hoài Thanh Vĩ <101490290+thanhvidev@users.noreply.github.com>
2024-11-08 17:30:29 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+const { Client } = require('discord.js-selfbot-v13');
+const { joinVoiceChannel } = require("@discordjs/voice");
+const client = new Client({ checkUpdate: false });
+
+const config = require(`${process.cwd()}/config.json`);
+
+client.on('ready', async () => {
+    console.log(`Đăng nhập vào ${client.user.tag}!`);
+
+    await joinVC(client, config);
+});
+
+client.on('voiceStateUpdate', async (oldState, newState) => {
+    const oldVoice = oldState.channelId;
+    const newVoice = newState.channelId;
+
+    if (oldVoice !== newVoice) {
+        if (!oldVoice) {
+        } else if (!newVoice) {
+            if (oldState.member.id !== client.user.id) return;
+            await joinVC(client, config);
+        } else {
+            if (oldState.member.id !== client.user.id) return;
+            if (newVoice !== config.Channel) {
+                await joinVC(client, config);
+            }
+        }
+    }
+});
+
+client.login(config.Token);
+
+async function joinVC(client, config) {
+    const guild = client.guilds.cache.get(config.Guild);
+    const voiceChannel = guild.channels.cache.get(config.Channel);
+    const connection = joinVoiceChannel({
+        channelId: voiceChannel.id,
+        guildId: guild.id,
+        adapterCreator: guild.voiceAdapterCreator,
+        selfDeaf: false,
+        selfMute: true
+    });
+}
